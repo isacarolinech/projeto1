cd6681556cc2614ddb01ed218db5130964e5a2f9
isacarolinech <141650652+isacarolinech@users.noreply.github.com>
2023-10-23 08:08:32 -0400

    Create javamusica.js

---
+++ javamusica.js
@@ -0,0 +1,52 @@
+export default [
+    {      
+      src: "/.MUSICAMP3/3 . Delacruz - Sunshine (Nonsense . Vol 1)_FEpuC98wcXE.mp3",
+      name: "dELACRUZ - Sunshine",
+      image: "./ImagensMusica/Sunshine.jpg"
+    },
+    {
+      src: "./MUSICAMP3/BK' - Planos part. Luccas Carlos (Gigantes)_V141wUSkTfk.mp3",
+      name: "Planos - BK",
+      image: "./ImagensMusica/Planos.jpg"
+    },
+    {
+      src: "./MUSICAMP3/Charlie Brown Jr. - Dias De Luta, Dias De Glória_6eEOegzrwJg.mp3",
+      name: "Dias De Luta, Dias De Glória - Charlie Brown Jr",
+      image: "./ImagensMusica/DiasDeLutaDiasDeGloria.jpg"
+    },
+    {
+      src: "./MUSICAMP3/Cinnamon Girl_DCYmJDO2_IE.mp3",
+      name: "Cinnamon Girl - Lana",
+      image: "./ImagensMusica/Cinnamon.webp"
+    },
+    {
+      src: "./MUSICAMP3/Dr. Dog _Where'd All The Time Go___bmZQpbNK7t4.mp3",
+      name: "Where'd All The Time Go",
+      image: "./ImagensMusica/Where'd All The Time Go.jpg"
+    },
+    {
+      src: "./MUSICAMP3/Matuê - Anos Luz 🌠_m226f2reF28.mp3",
+      name: "Anos Luz - Matuê",
+      image: "./ImagensMusica/Anos Luz - Matuê.webp"
+    },
+    {
+      src: "./MUSICAMP3/Meddle About_0n0nXfyfrs0.mp3",
+      name: "Meddle About - Chase Atlantic",
+      image: ".ImagensMusica/Meddle About - Chase Atlantic.webp"
+    },
+    {
+      src: "./MUSICAMP3/The Hills_G5XpJP7f_SE.mp3",
+      name: "The Hills - The Weeknd",
+      image: "./ImagensMusica/The Hills - The Weeknd.jpg"
+    },
+    {
+      src: "./MUSICAMP3/Tyler, The Creator - NEW MAGIC WAND (feat. Santigold & Jessy Wilson)_mdCyzJT59nw.mp3",
+      name: "NEW MAGIC WAND - Tyler, The Creator",
+      image: "./ImagensMusica/NEW MAGIC WAND - Tyler, The Creator.jpg"
+    },
+    {
+      src: "./Arctic Monkeys - Do I Wanna Know_ (Official Video)_bpOSxM0rNPM.mp3",
+      name: "Do I Wanna Know - Arctic Monkeys",
+      image: "./ImagensMusica/Do I Wanna Know - Arctic Monkeys.jpg"
+    },
+  ];
